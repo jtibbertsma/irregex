c8c384741a6408e64d6c71b2e35d2797b2a46af5
Joseph Tibbertsma <josephtibbertsma@gmail.com>
2015-10-21 00:03:43 -0700

    Add some detail to the lexer

--- spec/javascripts/parserSpec.js
+++ spec/javascripts/parserSpec.js
@@ -4,4 +4,47 @@ describe("parser", function () {
   it('returns an array containing an empty array if an empty string is given', function () {
     expect(parser.parse('')).toEqual([[]]);
   });
+
+  describe('literals', function () {
+
+    it('should only generate one node for a literal string', function () {
+      var tree = parser.parse('literal');
+      expect(tree.length).toEqual(1);
+      expect(tree[0].length).toEqual(1);
+      expect(tree[0][0].value).toEqual("literal");
+    });
+  });
+
+  describe('alternation', function () {
+
+    it('generates multiple paths for alternation', function () {
+      var tree = parser.parse('one|two|three');
+      expect(tree.length).toEqual(3);
+    });
+  });
+
+  describe('repetition', function () {
+    var simpleTree;
+
+    beforeEach(function () {
+      simpleTree = parser.parse('abcd*')[0];
+    });
+
+    it('detects repetition', function () {
+      var repetition = simpleTree[simpleTree.length-1].repetition.range;
+      expect(repetition[0]).toEqual(0);
+      expect(repetition[1]).toBeGreaterThan(100000);
+    });
+
+    it('detects modifiers', function () {
+      expect(parser.parse('a??')[0][0].repetition.type).toEqual('lazy');
+    });
+
+    describe('the lexer', function () {
+
+      it('doesn\'t report a tCHARLIT just before a repetition symbol', function () {
+        expect(simpleTree[0].value).toEqual('abc');
+      });
+    });
+  });
 });
